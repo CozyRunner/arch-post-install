8d70de9daa98e7e9fd249efd77d73ae784312c7d
Sachin S. <github@sachins.slmail.com>
2026-05-12 21:55:39 +0530

    fix: dotfiles config errors

--- dotfiles/fish/conf.d/theme.fish
+++ dotfiles/fish/conf.d/theme.fish
@@ -1,4 +1,4 @@
-# Base colors                                                                 
+# Base colors
 set -U fish_color_normal '#d3dae3'
 set -U fish_color_command '#71f79f'
 set -U fish_color_quote '#fdbc4b'
@@ -8,7 +8,7 @@ set -U fish_color_error '#ff6b6b'
 set -U fish_color_param '#d3dae3'
 set -U fish_color_comment '#8b9199'
 
-# UI states                                                                   
+# UI states
 set -U fish_color_match --background='#7fbfff'
 set -U fish_color_selection '#1a1c20' --bold --background='#fdbc4b'
 set -U fish_color_search_match '#1a1c20' --background='#fdbc4b'
@@ -17,7 +17,7 @@ set -U fish_color_operator '#0088a8'
 set -U fish_color_escape '#0088a8'
 set -U fish_color_autosuggestion '#8b9199'
 
-# Prompt                                                                      
+# Prompt
 set -U fish_prompt_color_logo cyan
 set -U fish_prompt_color_time purple
 set -U fish_prompt_color_user green
